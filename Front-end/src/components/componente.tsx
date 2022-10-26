3caf1b3142bf955632e4519e78c99681d4d2d077
Doragon <berna2022@gmail.com>
2022-10-26 09:13:08 -0300

    Inicio repo con sequelize

--- Front-end/src/components/componente.tsx
+++
@@ -1,9 +0,0 @@
-const Componente1 = () => (
-    <div id="results" className="search-results">
-      <h1>
-        Colocar funcionalidad aquí
-      </h1>
-    </div>
-  )
-
-export default Componente1;
--- Front-end/src/types.d.ts
+++
@@ -1,6 +0,0 @@
-export interface test{
-    pepe: string
-    hola: number
-    jaja: any
-    jue?: boolean | null | never
-}
