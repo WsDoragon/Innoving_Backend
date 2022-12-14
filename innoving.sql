c3c6accdca2c53035696cf1fa22190020b3d89dd
Nicol-Huaiquil <72150599+Nicol-Huaiquil@users.noreply.github.com>
2022-12-14 10:57:04 -0300

    Cambio base de datos
    un usuario con contraseña encriptada

--- innoving.sql
+++ innoving.sql
@@ -151,7 +151,7 @@ CREATE TABLE `rol_usuario` (
 
 LOCK TABLES `rol_usuario` WRITE;
 /*!40000 ALTER TABLE `rol_usuario` DISABLE KEYS */;
-INSERT INTO `rol_usuario` VALUES ('20318537-5',1),('20318537-5',2),('20318537-5',3),('a',1),('a',2),('a',3),('ab',1),('ab',2),('ab',3),('abc',2),('b',4),('c',3),('d',2),('d',3),('jorge',4),('juanito',4);
+INSERT INTO `rol_usuario` VALUES ('a-0',1), ('a-0',2),('a-0',3);
 /*!40000 ALTER TABLE `rol_usuario` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -181,6 +181,6 @@ CREATE TABLE `usuario` (
 
 LOCK TABLES `usuario` WRITE;
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES ('20318537-5','Jorge','Gonzalez','jorge.gonzalez04@alumnos.uach.cl','1f40fc92da241694750979ee6cf582f2d5d7d28e18335de05abc54d0560e0f5302860c652bf08d560252aa5e74210546f369fbbbce8c12cfc7957b2652fe9a75',1,NULL),('a','test1','t1','test1@test.com','1f40fc92da241694750979ee6cf582f2d5d7d28e18335de05abc54d0560e0f5302860c652bf08d560252aa5e74210546f369fbbbce8c12cfc7957b2652fe9a75',1,NULL),('ab','ab','ab','ab','2d408a0717ec188158278a796c689044361dc6fdde28d6f04973b80896e1823975cdbf12eb63f9e0591328ee235d80e9b5bf1aa6a44f4617ff3caf6400eb172d',1,NULL),('abc','abc','abc','abc','ddaf35a193617abacc417349ae20413112e6fa4e89a97ea20a9eeee64b55d39a2192992a274fc1a836ba3c23a3feebbd454d4423643ce80e2a9ac94fa54ca49f',1,NULL),('b','test2','t2','test2@test.com','5267768822ee624d48fce15ec5ca79cbd602cb7f4c2157a516556991f22ef8c7b5ef7b18d1ff41c59370efb0858651d44a936c11b7b144c48fe04df3c6a3e8da',1,NULL),('c','c','c','c','acc28db2beb7b42baa1cb0243d401ccb4e3fce44d7b02879a52799aadff541522d8822598b2fa664f9d5156c00c924805d75c3868bd56c2acb81d37e98e35adc',1,NULL),('d','d','d','d','48fb10b15f3d44a09dc82d02b06581e0c0c69478c9fd2cf8f9093659019a1687baecdbb38c9e72b12169dc4148690f87467f9154f5931c5df665c6496cbfd5f5',0,NULL),('jorge','jorge','jorge','jorge','9fc14a7597ce315e0a43bb0f1c4263f9669a9d632b93cde5ab869c3d4315c497de96c95839805a74627cb70cd944b147c4e71a83284c149a2ef5dfecbc3defdf',1,NULL),('juanito','juanito','juanito','juanito@gmail.com','8b81245f140944dfe6d2bb2e4cb186c9b1ad3add37cb4aa283bd6c054aeb56cda750a3719429a9f9e2db8b38325107c53ee2ecd295115677dea14a4ca81f531b',0,NULL);
+INSERT INTO `usuario` VALUES ('a-0','carlitos','torrez','carlitos@gmail.com',"1f40fc92da241694750979ee6cf582f2d5d7d28e18335de05abc54d0560e0f5302860c652bf08d560252aa5e74210546f369fbbbce8c12cfc7957b2652fe9a75",1,NULL);
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
