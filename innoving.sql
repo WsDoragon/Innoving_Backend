83d73e6ab6a18ae5bc5c87c49cfdee3fad38f2e7
heru-nan <hrwgallardo@gmail.com>
2022-12-21 13:25:45 -0300

    add changes to evidencias tables

--- innoving.sql
+++ innoving.sql
@@ -79,17 +79,18 @@ CREATE TABLE Indicadores_Variables (
 );
 create table publicacion(
     publicacion_id int not null AUTO_INCREMENT primary key,
-	issn_doi varchar(100) not null,
+	  issn_doi varchar(100) not null,
     titulo varchar(100),
     autores varchar(100),
     revista varchar(100),
     autores_extranjeros TINYINT(1),
+    validado TINYINT(1),
     indexacion varchar(100), 
-    anio varchar(100),
-    citaciones varchar(300),
+    anio date,
     clasificacion varchar(100),
     disciplina varchar(100)
 );
+
 CREATE TABLE Variables_Publicaciones (
   id INT PRIMARY KEY AUTO_INCREMENT,
   valor INT NOT NULL,
