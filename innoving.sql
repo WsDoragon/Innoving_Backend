cb871d4f35b86ae816d47557d6c86aedef28b418
MillarCD <diego12millar@gmail.com>
2022-12-21 09:37:50 -0300

    modificacion a la tabla publicaciones

--- innoving.sql
+++ innoving.sql
@@ -62,7 +62,6 @@ CREATE TABLE IF NOT EXISTS `metas` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
-CREATE TABLE Estados (estado VARCHAR(100) NOT NULL PRIMARY KEY);
 
 
 CREATE TABLE Variables (
@@ -77,9 +76,13 @@ CREATE TABLE Indicadores_Variables (
   FOREIGN KEY (id_indicador) REFERENCES indicadores(NumeroIndicador),
   FOREIGN KEY (id_variable) REFERENCES Variables(id)
 );
+
+CREATE TABLE Estados (estado VARCHAR(100) NOT NULL PRIMARY KEY);
+INSERT INTO Estados VALUES ("rechazado"), ("verificado");
+
 create table publicacion(
     publicacion_id int not null AUTO_INCREMENT primary key,
-	issn_doi varchar(100) not null,
+    issn_doi varchar(100) not null,
     titulo varchar(100),
     autores varchar(100),
     revista varchar(100),
@@ -88,8 +91,12 @@ create table publicacion(
     anio varchar(100),
     citaciones varchar(300),
     clasificacion varchar(100),
-    disciplina varchar(100)
+    disciplina varchar(100),
+    estado VARCHAR(100),
+    FOREIGN KEY (estado) REFERENCES Estados(estado)
 );
+
+
 CREATE TABLE Variables_Publicaciones (
   id INT PRIMARY KEY AUTO_INCREMENT,
   valor INT NOT NULL,
